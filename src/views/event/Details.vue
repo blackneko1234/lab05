ee65efb9af4a8973241426c76f310951ac945d2e
Decha_L <blackneko1234@hotmail.com>
2021-07-29 15:53:00 +0700

    1.6
    1.6

--- src/views/event/Details.vue
+++ src/views/event/Details.vue
@@ -1,10 +1,14 @@
 <template>
+  <div id="flashMessage" v-if="GStore.flashEdit">
+    {{ GStore.flashEdit }}
+  </div>
   <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
   <p>{{ event.description }}</p>
 </template>
 
 <script>
 export default {
-  props: ['event']
+  props: ['event'],
+  inject: ['GStore']
 }
 </script>
--- src/views/event/Edit.vue
+++ src/views/event/Edit.vue
@@ -1,9 +1,27 @@
 <template>
   <p>Edit the event here</p>
+  <button @click="edit">Edit Me</button>
 </template>
 
 <script>
 export default {
-  props: ['event']
+  props: ['event'],
+  inject: ['GStore'], //<--- Inject the Global store
+  methods: {
+    edit() {
+      // Assuming successful API call to register them
+      // Set a flash message to appear ono the next page loaded which says
+      // 'You are successfully registered for ' + this.event.title
+      this.GStore.flashEdit = 'The data has been updated'
+      setTimeout(() => {
+        // After 3 seconds remove it
+        this.GStore.flashEdit = ''
+      }, 3000)
+      this.$router.push({
+        name: 'EventDetails',
+        params: { id: this.event.id }
+      })
+    }
+  }
 }
 </script>
